aa77b45a654a0b2cdfc7c769585232e6db7ddc29
stiven lovera <stivenlovera@gmail.com>
2025-09-19 11:00:25 -0400

    menu inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,84 @@
+import { Route } from '@angular/router';
+import { initialDataResolver } from 'app/app.resolvers';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+
+    // Redirect empty path to '/dashboards/project'
+    { path: '', pathMatch: 'full', redirectTo: 'dashboards/project' },
+
+    // Redirect signed-in user to the '/dashboards/project'
+    //
+    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'dashboards/project' },
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            { path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes') },
+        ]
+    },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            { path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.routes') },
+            { path: 'unlock-session', loadChildren: () => import('app/modules/auth/unlock-session/unlock-session.routes') }
+        ]
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            { path: 'home', loadChildren: () => import('app/modules/landing/home/home.routes') },
+        ]
+    },
+
+    // Admin routes
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        resolve: {
+            initialData: initialDataResolver
+        },
+        children: [
+
+           // Dashboards
+            {path: 'dashboards', children: [
+                {path: 'project', loadChildren: () => import('app/modules/admin/dashboards/project/project.routes')},
+            ]},
+
+            // 404 & Catch all
+            { path: '404-not-found', pathMatch: 'full', loadChildren: () => import('app/modules/admin/pages/error/error-404/error-404.routes') },
+            { path: '**', redirectTo: '404-not-found' }
+        ]
+    }
+];
